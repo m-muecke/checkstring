88f00793920499d4d38c22eef7d88568037476ab
Maximilian Muecke <muecke.maximilian@gmail.com>
2025-04-11 23:58:08 +0200

    docs: document

--- R/string.R
+++ R/string.R
@@ -1,21 +1,33 @@
+#' Check if an argument is a email address string
+#'
+#' @param x (`any`) object to check.
 #' @export
 is_email <- function(x) {
   regex <- "^(?!\\.)(?!.*\\.\\.)([A-Z0-9_'+\\-\\.]*[A-Z0-9_+\\-])@([A-Z0-9][A-Z0-9\\-]*\\.)+[A-Z]{2,}$" # nolint
   is_string(x) && grepl(regex, x, ignore.case = TRUE, perl = TRUE)
 }
 
+#' Check if an argument is an UUID string
+#'
+#' @param x (`any`) object to check.
 #' @export
 is_uuid <- function(x) {
   regex <- "^[0-9a-f]{8}\\b-[0-9a-f]{4}\\b-[0-9a-f]{4}\\b-[0-9a-f]{4}\\b-[0-9a-f]{12}$" # nolint
   is_string(x) && grepl(regex, x, ignore.case = TRUE, perl = TRUE)
 }
 
+#' Check if an argument is a base64 string
+#'
+#' @param x (`any`) object to check.
 #' @export
 is_base64 <- function(x) {
   regex <- "^([0-9a-zA-Z+/]{4})*((([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?)$"
   is_string(x) && grepl(regex, x, perl = TRUE)
 }
 
+#' Check if an argument is base64url string
+#'
+#' @param x (`any`) object to check.
 #' @export
 is_base64url <- function(x) {
   regex <- "^([0-9a-zA-Z\\-_]{4})*((([0-9a-zA-Z\\-_]{2}(==)?)|([0-9a-zA-Z\\-_]{3}(=)?))?)$" # nolint
