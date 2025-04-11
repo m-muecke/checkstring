8ad22d2d52be2ea8e1bf6005de1ef9770a2c4db7
Maximilian Muecke <muecke.maximilian@gmail.com>
2025-04-11 23:22:20 +0200

    init

--- R/string.R
+++ R/string.R
@@ -1,3 +1,7 @@
-is_email <- function() {
-  .NotYetImplemented("is_email")
+is_string <- function(x) {
+  is.character(x) && length(x) == 1L && !is.na(x)
+}
+
+is_email <- function(x) {
+  is_string(x)
 }
